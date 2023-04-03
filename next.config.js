2a01d5845baaa441555e2d3cd72993000bad9479
Muhammad Hamza Mukhtar <rhmza4@gmail.com>
2023-04-03 19:27:13 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  env: {
+    CONTENTFUL_SPACE_ID: "repxciuckvtc",
+    CONTENTFUL_ACCESS_KEY: "fXHlB98zC-MNxKjvp6Ybn4t5mm6noRmBPo7_WTzTgDE",
+  },
+  images: {
+    domains: ["images.ctfassets.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
